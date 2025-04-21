23d86dd9f8d7a8b1086922fdb2facb36ba1106e3
varunriyer <vs7835@srmist.edu.in>
2025-04-21 11:20:23 +0530

    Implement iterative binary search function with example usage

--- Searching.js
+++ Searching.js
@@ -15,3 +15,34 @@
     console.log(linear_search(my_arr, my_key1));
     console.log(linear_search(my_arr, my_key2));
 }
+
+// Binary Search 
+
+//Iterative Binary Search 
+{
+    function iter_binary_search(arr, key) {
+        let low = 0;
+        let high = arr.length - 1;
+        let mid;
+        while (high >= low) {
+            mid = Math.floor((low + high) / 2)
+
+            if (key < arr[mid]) {
+                high = mid - 1;
+            }
+            if (key > arr[mid]) {
+                low = mid + 1;
+            }
+            if (key === arr[mid]) {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+    let my_arr = [10, 20, 30, 40, 50];
+    let my_key1 = 40;
+    let my_key2 = 17;
+    console.log(iter_binary_search(my_arr, my_key1));
+    console.log(iter_binary_search(my_arr, my_key2));
+}
