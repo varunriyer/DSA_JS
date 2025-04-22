7b1b4e7db21b0a6b90669c55a5324df10e24bb91
varunriyer <vs7835@srmist.edu.in>
2025-04-22 10:42:58 +0530

    add example usage for linkedList class

--- Linked_List.js
+++ Linked_List.js
@@ -30,3 +30,9 @@ class linkedList {
         console.log(result + 'null');
     }
 }
+let list = new linkedList();
+list.append(10);
+list.append(20);
+list.append(30);
+list.append(40);
+list.printList();
