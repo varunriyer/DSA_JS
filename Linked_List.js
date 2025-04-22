083a897b3f50822c4ca901f8a1b6b3fa1345760d
varunriyer <vs7835@srmist.edu.in>
2025-04-22 10:36:02 +0530

    implement append method for linked list

--- Linked_List.js
+++ Linked_List.js
@@ -8,4 +8,16 @@ class linkedList {
     constructor() {
         this.head = null;
     }
+    append(value) {
+        let newnode = new Node(value);
+        if (!this.head) {
+            this.head = newnode;
+            return;
+        }
+        let current = this.head;
+        while (current.next) {
+            current = current.next;
+        }
+        current.next = newnode;
+    }
 }
