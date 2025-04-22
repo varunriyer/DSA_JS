26a7dd44bf9b2a9fe5a1defa27ad75ae170179b6
varunriyer <vs7835@srmist.edu.in>
2025-04-22 10:30:57 +0530

    add linked list implementation with Node class

---
+++ Linked_List.js
@@ -0,0 +1,11 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null
+    }
+}
+class linkedList {
+    constructor() {
+        this.head = null;
+    }
+}
