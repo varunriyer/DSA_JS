030d8fc0ddac66d0d7190e9224f9256b2e567705
varunriyer <vs7835@srmist.edu.in>
2025-04-24 15:50:26 +0530

    Implement append method in LinkedList class for adding new nodes

--- rotate_a_linked_list.js
+++ rotate_a_linked_list.js
@@ -8,4 +8,17 @@ class LinkedList {
     constructor() {
         this.head = null;
     }
+    append(value) {
+        let newNode = new Node(value);
+        if (this.head === null) {
+            this.head = newNode;
+            return;
+        }
+        let current = this.head;
+        while (current.next !== null) {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+
 }
