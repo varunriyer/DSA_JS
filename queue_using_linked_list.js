23ba24fdbf130086cae1800971a3fe3e7cb10d87
varunriyer <vs7835@srmist.edu.in>
2025-04-24 15:08:34 +0530

    Add enqueue method to Queue class for adding elements

--- queue_using_linked_list.js
+++ queue_using_linked_list.js
@@ -8,5 +8,15 @@ class Queue {
     constructor() {
         this.head = null
     }
-
+    enqueue(value) {
+        let newNode = new Node(value);
+        if (this.head === null) {
+            this.head = newNode;
+        }
+        let current = this.head;
+        while (current.next !== null) {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
 }
