c2e67a5891b507770063e0498538b6b20c66943d
varunriyer <vs7835@srmist.edu.in>
2025-04-25 14:33:41 +0530

    Add CircularLinkedList class implementation with constructor

--- circular_linked_list.js
+++ circular_linked_list.js
@@ -4,4 +4,13 @@ class Node {
         this.null = null
     }
 }
+class CircularLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+
+}
 
