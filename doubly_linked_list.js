75604165b48bfe458c4033577cc45077d185fd1b
varunriyer <vs7835@srmist.edu.in>
2025-04-25 10:29:39 +0530

    Implement insertAtBeginning method for DoublyLinkedList class

--- doubly_linked_list.js
+++ doubly_linked_list.js
@@ -12,5 +12,19 @@ class DoublyLinkedList {
         this.tail = null;
         this.length = 0;
     }
+    insertAtBeginning(value) {
+        let newNode = new Node(value);
+        if (this.head === null) {
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        else {
+            newNode.next = this.head;
+            this.head.prev = newNode;
+            this.head = newNode;
+        }
+        this.length++
+    }
+
 
 }
