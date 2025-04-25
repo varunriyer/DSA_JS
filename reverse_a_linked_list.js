526e5791ae6183fa12b30ff2c8828dfd433a750c
varunriyer <vs7835@srmist.edu.in>
2025-04-25 10:17:54 +0530

    Implement linked list with append, print, size, and reverse methods

---
+++ reverse_a_linked_list.js
@@ -0,0 +1,75 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+class LinkedList {
+    constructor() {
+        this.head = null
+    }
+    append(value) {
+        let newNode = new Node(value);
+        if (this.head === null) {
+            this.head = newNode;
+            return;
+        }
+        let current = this.head;
+        while (current.next !== null) {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+    printList() {
+        if (this.head === null) {
+            console.log("List is empty");
+            return;
+        }
+        let result = "";
+        let current = this.head;
+        while (current !== null) {
+            result += current.value + "-->";
+            current = current.next;
+        }
+        console.log(result + "null");
+    }
+    size() {
+        if (this.head === null) {
+            console.log("List is empty");
+            return 0;
+        }
+        let current = this.head;
+        let count = 0;
+        while (current !== null) {
+            count++;
+            current = current.next;
+        }
+        return count;
+    }
+    reverse() {
+        let prev = null;
+        let current = this.head;
+
+        while (current != null) {
+            let nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+        this.head = prev;
+
+    }
+}
+
+let myList = new LinkedList();
+myList.printList();
+console.log(myList.size());
+myList.append(2);
+myList.append(4);
+myList.append(6);
+myList.append(8);
+myList.append(10);
+console.log(myList.size())
+myList.printList();
+myList.reverse();
+myList.printList();
