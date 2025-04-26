281c5547800d1cc7294ead4ee632592df9fe8bf8
varunriyer <vs7835@srmist.edu.in>
2025-04-26 11:09:51 +0530

    Rename Node class to TreeNode and implement BFS method

--- trees.js
+++ trees.js
@@ -1,13 +1,31 @@
-class Node {
+class TreeNode {
     constructor(value) {
         this.value = value;
         this.left = null;
         this.right = null
     }
+    BFS() {
+        if (!root) {
+            console.log("No elements to display");
+            return;
+        }
+        let queue = [root];
+        while (queue.length) {
+            let item = queue.shift();
+
+            console.log(item);
+
+            if (item.left) queue.push(item.left)
+        }
+    }
 }
 
-let root = new Node(2);
-root.left = new Node(1);
-root.right = new Node(3);
+let root = new TreeNode(5);
+root.left = new TreeNode(3);
+root.right = new TreeNode(7);
+root.left.left = new TreeNode(2);
+root.left.right = new TreeNode(4);
+root.right.left = new TreeNode(6);
+root.right.right = new TreeNode(9);
 
 console.log(root);
