f29c884854acd40e83cc7eca3ad7702f301b12f4
varunriyer <vs7835@srmist.edu.in>
2025-04-26 11:32:05 +0530

    Implement Tree class with BFS and cleanBFS methods for tree traversal

--- trees.js
+++ trees.js
@@ -4,7 +4,34 @@ class TreeNode {
         this.left = null;
         this.right = null
     }
-    BFS() {
+}
+
+class Tree {
+    constructor(root = null) {
+        this.root = root
+    }
+
+    cleanBFS(root) {
+        if (!root) {
+            console.log("No Tree elements present");
+            return;
+        }
+        let queue = [root];
+        let ans = [];
+
+        while (queue.length) {
+            let len = queue.length, level = [];
+            for (let i = 0; i < len; i++) {
+                let item = queue.shift();
+                level.push(item.value);
+                if (item.left) queue.push(item.left);
+                if (item.right) queue.push(item.right);
+            }
+            ans.push(level);
+        }
+        return ans;
+    }
+    BFS(root) {
         if (!root) {
             console.log("No elements to display");
             return;
@@ -20,6 +47,7 @@ class TreeNode {
     }
 }
 
+
 let root = new TreeNode(5);
 root.left = new TreeNode(3);
 root.right = new TreeNode(7);
@@ -29,3 +57,8 @@ root.right.left = new TreeNode(6);
 root.right.right = new TreeNode(9);
 
 console.log(root);
+
+let tree = new Tree(root);
+
+console.log(tree.cleanBFS(root));
+console.log(tree.BFS(root));
