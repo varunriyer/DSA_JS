6322017566012d6aa700fa21da5f06d6fcd82051
varunriyer <vs7835@srmist.edu.in>
2025-04-29 11:38:40 +0530

    Add examples for creating and logging empty objects

--- Objects.js
+++ Objects.js
@@ -5,3 +5,16 @@
     console.log(obj);
 }
 
+// You can store empty objects 
+
+{
+    let obj1 = new Object();
+    console.log(obj1);
+
+    let obj2 = new Object(undefined);
+    console.log(obj2);
+
+    let obj3 = new Object(null);
+    console.log(obj3);
+}
+
